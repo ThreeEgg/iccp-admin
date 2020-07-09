f1ffdb4ebc1672f9a7e90506f2c668305660c8c3
lichen <office_lichen@163.com>
2020-07-09 10:20:07 +0800

    修复退出登录白条的问题

--- src/components/HeaderDropdown/index.jsx
+++ src/components/HeaderDropdown/index.jsx
@@ -4,7 +4,11 @@ import classNames from 'classnames';
 import './index.less';
 
 const HeaderDropdown = ({ overlayClassName: cls, ...restProps }) => (
-  <Dropdown overlayClassName={classNames('container', cls)} {...restProps} />
+  <Dropdown
+    overlayStyle={{ height: 'auto', overflow: 'initial' }}
+    overlayClassName={classNames('container', cls)}
+    {...restProps}
+  />
 );
 
 export default HeaderDropdown;
